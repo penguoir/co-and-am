1f9bf411ecbce5a5a27bf97d4ea3e77fd9947552
penguoir <games@marash.net>
2017-08-20 23:06:37 +0100

    Convert let to var

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
-let express = require('express')
-let router = express.Router()
+var express = require('express')
+var router = express.Router()
 
 router.get('/', function(req, res) {
   res.status(200)
